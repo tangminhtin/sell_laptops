ff3f1098a72c31bbca12df9a1bf223f07dc56ad3
Minh Tin Tang <minhtintang@gmail.com>
2021-05-27 21:50:10 +0700

    finished

--- js/script.js
+++ js/script.js
@@ -13,21 +13,37 @@ function menuToggle() {
 var productImg = document.getElementById("productImg");
 var smallImg = document.getElementsByClassName("small-img");
 
-smallImg[0].onclick = function() {
-  productImg.src = smallImg[0].src;
+if (smallImg.length !== 0) {
+  smallImg[0].onclick = function () {
+    productImg.src = smallImg[0].src;
+  };
+
+  smallImg[1].onclick = function () {
+    productImg.src = smallImg[1].src;
+  };
+
+  smallImg[2].onclick = function () {
+    productImg.src = smallImg[2].src;
+  };
+
+  smallImg[3].onclick = function () {
+    productImg.src = smallImg[3].src;
+  };
 }
 
-smallImg[1].onclick = function() {
-  productImg.src = smallImg[1].src;
+// js for toggle form
+var loginForm = document.getElementById("loginForm");
+var regForm = document.getElementById("regForm");
+var indicator = document.getElementById("indicator");
+
+function register() {
+  regForm.style.transform = "translateX(0px)";
+  loginForm.style.transform = "translateX(0px)";
+  indicator.style.transform = "translateX(120px)";
 }
 
-smallImg[2].onclick = function() {
-  productImg.src = smallImg[2].src;
+function login() {
+  regForm.style.transform = "translateX(300px)";
+  loginForm.style.transform = "translateX(300px)";
+  indicator.style.transform = "translateX(5px)";
 }
-
-smallImg[3].onclick = function() {
-  productImg.src = smallImg[3].src;
-}
-
-
-
